c705cb4fa09638dbdb7c7ed0aacb81b17fbefe85
Eileen <es@blueyonder.co.uk>
2024-03-05 13:45:06 +0000

    new text

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Eileen"));
+        System.out.println(sayHelloToSomeone("EileenS"));
         System.out.println(sayGoodbye());
     }
 
